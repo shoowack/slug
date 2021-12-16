59802747b150080514568d1cff69c81c279925f5
shoowack <isuvak@gmail.com>
2021-12-16 09:22:38 -0500

    adding input fields and logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [slug, setSlug] = useState("");
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <table style={{ margin: "auto" }}>
+        <tr>
+          <td style={{ textAlign: "right" }}>
+            <label>Name: </label>
+          </td>
+          <td>
+            <input
+              onChange={(e) =>
+                setSlug(
+                  e.target.value
+                    .replace(/[^-a-zA-Z0-9\s+]+/gi, "")
+                    .replace(/\s+/gi, "-")
+                    .toLowerCase()
+                )
+              }
+            />
+          </td>
+        </tr>
+        <tr>
+          <td style={{ textAlign: "right" }}>
+            <label>Slug: </label>
+          </td>
+          <td>
+            <input value={slug} />
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
